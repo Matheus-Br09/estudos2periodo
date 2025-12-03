18d27a27f9e399f22a1e10bc4215b32ffb733f25
Matheus <matheusps3.br06@gmail.com>
2025-12-03 15:20:52 -0300

    03/12 Revisões de prova

--- Treino/Treino07/src/main.java
+++ Treino/Treino07/src/main.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
         try {
             System.out.print("Digite um valor para o pagamento: R$");
